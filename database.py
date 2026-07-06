b55b63afd8c89df7bf65a6774374dd0fa57097c2
srushtirakhonde <srushtirakhone15@gamil.com>
2026-07-06 14:26:58 +0530

    prepare for deployment

--- database.py
+++ database.py
@@ -1,5 +1,7 @@
 import sqlite3
 
+from flask import app
+
 DB_PATH = "college.db"
 
 def get_db():
@@ -39,6 +41,6 @@ def init_db():
     conn.commit()
     conn.close()
 
-
+init_db()
 if __name__ == "__main__":
-    init_db()
+ app.run(debug=True)
